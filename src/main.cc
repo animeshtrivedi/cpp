65c65afdc7edf8410a1b306de32016b2dd45cfcf
Animesh Trivedi <animesh.trivedi@gmail.com>
2018-10-12 11:37:50 +0200

    initial commit for cpp examples

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <iostream>
+int main(int argc, char **argv) {
+  std::cout << "__cplusplus version is : " << __cplusplus << std::endl;
+  return 0;
+}
